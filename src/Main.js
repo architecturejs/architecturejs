ac4aa3a9ece5b5372dc2b5a70076efdddcc3650e
jero <aneurysmjs@outlook.com>
2019-09-09 13:25:37 -0500

    refactor(main): remove jsx extension
    use just .js

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+// @flow
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+// import main CSS styles
+import 'styles';
+
+import Landing from './pages/Landing/Landing';
+
+class MainComponent extends Component {
+
+  render() {
+    return (
+      <Router>
+        <main>
+          <div>
+            <Route exact path="/" component={Landing} />
+          </div>
+        </main>
+      </Router>
+    );
+  }
+  
+}
+
+ReactDom.render(<MainComponent />, document.querySelector('#app'));
